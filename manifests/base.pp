91d4f453e1e126e41e382801a2130b760b99fa0a
cleverlight <github.account@cleveraspaint.com>
2013-05-23 18:16:52 +0100

    first stab at a mantis source download, needs testing

--- manifests/base.pp
+++ manifests/base.pp
@@ -17,10 +17,27 @@ class domantis::base (
   $db_port = '3306',
   $db_grants = ['all'],
 
+  # install directory
+  $target_dir = '/var/www/html',
+
   # end of class arguments
   # ----------------------
   # begin class
 
 ) {
+
+  # name of first directory in downloaded zip
+  $target_name = 'mantisbt'
+  $starts_with = $target_name
+
+  # create <target_dir>/<target_name> folder from downloaded tar (then clean up tar) and remove version number
+  exec { 'install-mantis-source' :
+    path    => '/usr/bin:/bin:',
+    command => "bash -c \"wget http://sourceforge.net/projects/mantisbt/files/latest/download -O ${target_dir}/mantis-latest.tar.gz && cd ${target_dir} && tar xzf ${target_dir}/mantis-latest.tar.gz > /dev/null 2>&1 && rm ${target_dir}/mantis-latest.tar.gz && mv ${starts_with}* ${target_name}\"",
+    user => $user,
+    group => $group,
+    onlyif  => "test ! -d ${target_dir}/${target_name}",
+  }
+
 
 }
